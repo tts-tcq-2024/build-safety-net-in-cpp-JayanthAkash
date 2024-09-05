4a7e65dd6c5897c9ffc5d1fcd796a68f8346761a
JayanthAkash <jayanthakash.ve@gmail.com>
2024-09-05 20:54:14 +0530

    Reduced CCN

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,36 +1,64 @@
 #include "Soundex.h"
 #include <cctype>
+#include <string>
+#include <unordered_map>
+
+std::unordered_map<char, char> buildSoundexMap() {
+    return {
+        {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
+        {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'}, 
+        {'S', '2'}, {'X', '2'}, {'Z', '2'},
+        {'D', '3'}, {'T', '3'},
+        {'L', '4'},
+        {'M', '5'}, {'N', '5'},
+        {'R', '6'}
+    };
+}
 
 char getSoundexCode(char c) {
+    static std::unordered_map<char, char> soundexMap = buildSoundexMap();
     c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+    
+    if (soundexMap.find(c) != soundexMap.end()) {
+        return soundexMap[c];
+    }
+
+    return '0';
+}
+
+void addValidSoundexCode(std::string& soundex, char& prevCode, char currentCode) {
+    if (currentCode != '0' && currentCode != prevCode) {
+        soundex += currentCode;
+        prevCode = currentCode;
     }
 }
 
-std::string generateSoundex(const std::string& name) {
+void processNameForSoundex(const std::string& name, std::string& soundex, char& prevCode) {
+    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
+        char currentCode = getSoundexCode(name[i]);
+        addValidSoundexCode(soundex, prevCode, currentCode);
+    }
+}
+
+std::string generateCoreSoundex(const std::string& name) {
     if (name.empty()) return "";
 
     std::string soundex(1, toupper(name[0]));
     char prevCode = getSoundexCode(name[0]);
 
-    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
-        }
-    }
+    processNameForSoundex(name, soundex, prevCode);
 
+    return soundex;
+}
+
+void padSoundexCode(std::string& soundex) {
     while (soundex.length() < 4) {
         soundex += '0';
     }
+}
 
-    return soundex;
+std::string generateSoundex(const std::string& name) {
+    std::string coreSoundex = generateCoreSoundex(name);
+    padSoundexCode(coreSoundex);
+    return coreSoundex;
 }
